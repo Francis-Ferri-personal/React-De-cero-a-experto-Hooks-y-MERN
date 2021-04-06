edc424cda8461fa66b36140b8bc08fb3f2dc551c
Francis Ferri <francis.ferri@epn.com.ec>
2021-04-06 08:55:19 -0500

    GifExpertApp - Aplicación

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -22,7 +22,7 @@ const PrimeraApp = ( {saludo, subtitulo} ) => {
 };
 
 PrimeraApp.propTypes = {
-    saludo: PropTypes.string.isRequired,
+    saludo: PropTypes.string.isRequired
 };
 
 PrimeraApp.defaultProps = {
